ba8458f5efccb21644740b483c3c64dba3bcbdb1
crod2 <carlarodriguez90@gmail.com>
2019-05-24 11:40:59 +0200

    Create CharacterCard component's info

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -4,12 +4,24 @@ class CharacterCard extends React.Component {
   render() {
     const { charactersArr, id } = this.props;
     const characterCard = charactersArr[id];
+    const isAlive = characterCard.alive;
 
     return (
       <React.Fragment>
-        <h2>{characterCard.name}</h2>
-        <img src={characterCard.image} alt={`Imagen de ${characterCard.name}`}></img>
-        <h3>{characterCard.house}</h3>
+        <div className="card">
+          <h2>{characterCard.name}</h2>
+          <img src={characterCard.image} alt={`Imagen de ${characterCard.name}`}></img>
+          <ul className="card__details">
+            <li>{characterCard.house}</li>
+            <li>{characterCard.dateOfBirth}</li>
+            <li>{characterCard.patronus}</li>
+            {isAlive ? 
+            <li>Estado: VIVO</li> 
+            : 
+            <li>Estado: MUERTO</li>
+            }
+          </ul>
+        </div>
       </React.Fragment>
     )
   }
