f1f09032fd46eb7c480b400912a7490b123f8c09
crod2 <carlarodriguez90@gmail.com>
2019-05-24 09:17:10 +0200

    Create fetch function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
+import { fetchPotter } from './services/FetchPotter';
 
 class App extends React.Component {
+constructor(props) {
+  super(props) 
+
+  this.fetchCharacters = this.fetchCharacters.bind(this);
+}
+
+
+fetchCharacters() {
+  fetchPotter() 
+  .then(data => {
+    console.log(data)
+  })
+}
+
+componentDidMount() {
+  this.fetchCharacters();
+}
+
   render() {
     return (
       <div>
-        <p>Hola mundo</p>
+        <h1>Harry Potter Characters</h1>
       </div>
     )
   }
