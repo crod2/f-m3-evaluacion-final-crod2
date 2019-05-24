18b14d0c162b466204c297b708f9aa476135262f
crod2 <carlarodriguez90@gmail.com>
2019-05-24 08:59:05 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hola mundo</p>
+      </div>
+    )
+  }
+}
+
+export default App;
